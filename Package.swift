1f334dd5e408677745dd7a51c322c74aa8982018
runner <runner@Mac-1667020583536.local>
2022-10-29 07:49:59 +0000

    Update Package.swift for release 0.6.0

--- Package.swift
+++ Package.swift
@@ -24,8 +24,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "bdkFFI",
-            url: "'https://github.com/bitcoindevkit/bdk-swift/releases/download/0.6.0/bdkFFI.xcframework.zip'",
-            checksum: "3930ee1f816366c8ed971e231b44f2c1dca14ea32448283842b3d9275adfef1f"),
+            url: "https://github.com/bitcoindevkit/bdk-swift/releases/download/0.6.0/bdkFFI.xcframework.zip",
+            checksum: "32cbcdb2d339631154497ec801a9da2022310943eed46c37eac6f864b6490bb9"),
         .target(
             name: "BitcoinDevKit",
             dependencies: ["bdkFFI"]),
