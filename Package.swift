d7c151a7aef3741599fb3e50a873e63a4a0fda69
runner <runner@Mac-1667088203787.local>
2022-10-30 01:41:17 +0000

    Update Package.swift for release 0.6.0

--- Package.swift
+++ Package.swift
@@ -22,11 +22,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-//        .binaryTarget(
-//            name: "bdkFFI",
-//            url: "https://github.com/bitcoindevkit/bdk-swift/releases/download/0.3.0/bdkFFI.xcframework.zip",
-//            checksum: "7d4a2fdeb03fb3eff107e45ee3148dd9b67966406c82d6e3c19f653c27180cfd"),
-        .binaryTarget(name: "bdkFFI", path: "./bdkFFI.xcframework"),
+        .binaryTarget(
+            name: "bdkFFI",
+            url: "https://github.com/bitcoindevkit/bdk-swift/releases/download/0.6.0/bdkFFI.xcframework.zip",
+            checksum: "a36e34e19b1bfe9fd343dcc829284d7ee660eb3199d4fb0d9b365e8bf4dfdee3"),
         .target(
             name: "BitcoinDevKit",
             dependencies: ["bdkFFI"]),
